7a853afd85bd61ecb618643cd257b3271a6d78bd
Basin <basin@BasindeMacBook-Pro.local>
2016-01-09 22:20:52 +0800

    svm tuning script added.

---
+++ svm_tuning.py
@@ -0,0 +1,19 @@
+__author__ = 'basin'
+from sklearn.datasets import load_svmlight_file
+from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
+X, y = load_svmlight_file('./data/train.libsvm')
+tuned_parameters = [
+    {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+    {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+]
+print("# Tuning hyper-parameters for roc_auc with svc")
+clf = GridSearchCV(SVC(probability=True, class_weight='balanced', random_state=0),
+                   tuned_parameters, cv=5, scoring='roc_auc', n_jobs=10)
+clf.fit(X, y)
+print("Best parameters set found on development set:")
+print(clf.best_params_)
+print("Grid scores on development set:")
+for params, mean_score, scores in clf.grid_scores_:
+    print("%0.3f (+/-%0.03f) for %r"
+          % (mean_score, scores.std() * 2, params))
---
+++ xgbst_tuning.py
@@ -0,0 +1,20 @@
+__author__ = 'basin'
+from sklearn.datasets import load_svmlight_file
+from sklearn.grid_search import GridSearchCV
+
+import xgboost as xgb
+
+X, y = load_svmlight_file('./data/train.libsvm')
+tuned_parameters = {'reg_alpha': [1, 5, 10, 20, 50, 100],
+                    'scale_pos_weight': [1.0, 1542.0 / 13458.0, 23458.0 / 1542.0],
+                    'max_depth': [4, 6, 8, 10], 'n_estimators': [500, 1000, 2000, 5000]}
+print("# Tuning hyper-parameters for roc_auc with xgboost")
+clf = GridSearchCV(xgb.XGBClassifier(learning_rate=0.1, objective='binary:logistic'),
+                   tuned_parameters, cv=5, scoring='roc_auc', n_jobs=10)
+clf.fit(X, y)
+print("Best parameters set found on development set:")
+print(clf.best_params_)
+print("Grid scores on development set:")
+for params, mean_score, scores in clf.grid_scores_:
+    print("%0.3f (+/-%0.03f) for %r"
+          % (mean_score, scores.std() * 2, params))
